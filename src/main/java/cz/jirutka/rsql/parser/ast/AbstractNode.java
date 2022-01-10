5743948b25ee92df49158d820cbabc588e6ee52f
zero88 <sontt246@gmail.com>
2022-01-10 11:57:55 +0700

    [GHA] Update CI build [Document] Update CHANGELOG and README

--- src/main/java/cz/jirutka/rsql/parser/ast/AbstractNode.java
+++ src/main/java/cz/jirutka/rsql/parser/ast/AbstractNode.java
@@ -28,7 +28,7 @@ public abstract class AbstractNode implements Node {
     /**
      * Accepts the visitor, calls its visit() method and returns the result.
      * This method just calls {@link #accept(RSQLVisitor, Object)} with
-     * <tt>null</tt> as the second argument.
+     * {@code null} as the second argument.
      */
     public <R, A> R accept(RSQLVisitor<R, A> visitor) {
         return accept(visitor, null);
